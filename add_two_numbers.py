2c0cb574bde2c88f870f98d838db109bb5da510e
7Jojo7 <131754895+7Jojo7@users.noreply.github.com>
2026-02-04 02:31:39 +0530

    Add files via upload

---
+++ add_two_numbers.py
@@ -0,0 +1,11 @@
+# Simple program to add 2 integers
+
+# Get input from user
+num1 = int(input("Enter first integer: "))
+num2 = int(input("Enter second integer: "))
+
+# Add the two numbers
+sum = num1 + num2
+
+# Display the result
+print(f"The sum of {num1} and {num2} is {sum}")
